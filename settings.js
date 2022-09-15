3ca8dc6651f64d3cb2d0d1a8c5b0fb17aa7fa1cc
vikumking <113519723+vikumking@users.noreply.github.com>
2022-09-15 18:42:38 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,7 +11,7 @@ global.APIKeys = {
 	'https://zenzapis.xyz': 'your key',
 }
 
-global.alivemsg = `📤I am alive now😇
+global.alivemsg = `📤I am vikum now😇
 🤷‍♀️How can I help you?😉` //Costomize Alive Message (type your message in `` )
 
 
@@ -29,34 +29,34 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owner v card
-global.vcardowner = ['+380944814219'] // owner number
-global.ownername = "😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖❯❯" // owner name
-global.ytname = "" // yt chanel name
+global.vcardowner = ['+94741018442'] // owner number
+global.ownername = "ꪶ𝚅𝙸𝙺𝚄 𖠂 𝙻𝙸𝙾𝙽 ᴷᴵᴺᴳ_ꫂ" // owner name
+global.ytname = "ꪶ𝚅𝙸𝙺𝚄 𖠂 𝙻𝙸𝙾𝙽 ᴷᴵᴺᴳ_ꫂ" // yt chanel name
 global.socialm = "GitHub: prabhasha2006" // github or insta name
 global.location = "India, Mizoram, Aizawl" // location
 
 //bot body 
-global.alivepic = `https://i.ibb.co/cyyCjN7/20220803-081426.png` // alive picture \\ use "https://ibb.com" to upload photos. < 1 MB
+global.alivepic = `https://ibb.co/nQxcgj2` // alive picture \\ use "https://ibb.com" to upload photos. < 1 MB
 
-global.dragonchat = "false"  // chat bot  "true" / "false" (200 auto replies in this)
+global.dragonchat = "true"  // chat bot  "true" / "false" (200 auto replies in this)
 global.callblock = "false" // call block "true" / "false" ("true"  to block callers)
-global.antibad = "false" // To remove bad word senders
+global.antibad = "true" // To remove bad word senders
 global.antispam = "true" // To remove or block spammers
-global.antilink = "false" // To remove group link sharing peoples
-global.upsongvoice = "false" // true/false \\ bot can upload song as voice note (it use your data and storage bacause auto download) you can put "false" (anyone can't get songs as voice note / your data is not wasted)
+global.antilink = "true" // To remove group link sharing peoples
+global.upsongvoice = "true" // true/false \\ bot can upload song as voice note (it use your data and storage bacause auto download) you can put "false" (anyone can't get songs as voice note / your data is not wasted)
 global.welcom_msg = "true" // Welcome / Goodbye   [true/false]
 
 
-global.owner = ['+380944814219']
-global.ownertag = '+380944814219' //your tag number
-global.botname = '𝙍𝙀𝘿╺ 𝘿𝙍𝘼𝙂𝙊𝙉 - 𝘉𝘖𝘛' //ur bot name
-global.packname = "Bot Sticker" // sticker packname
-global.author = "𝙍𝙀𝘿╺ 𝘿𝙍𝘼𝙂𝙊𝙉" // sticker author
+global.owner = ['+94741018442']
+global.ownertag = '+94741018442' //your tag number
+global.botname = 'ꪶ𝚅𝙸𝙺𝚄 𖠂 𝙻𝙸𝙾𝙽 ᴷᴵᴺᴳ_ꫂ' //ur bot name
+global.packname = "ꪶ𝚅𝙸𝙺𝚄 𖠂 𝙻𝙸𝙾𝙽 ᴷᴵᴺᴳ_ꫂ" // sticker packname
+global.author = "ꪶ𝚅𝙸𝙺𝚄 𖠂 𝙻𝙸𝙾𝙽 ᴷᴵᴺᴳ_ꫂ" // sticker author
 global.dragonemoji = '🧩' // Menu emoji
-global.footer = '[ ◉ ʀᴇᴅ-ᴅʀᴀɢᴏɴ-ᴍᴅ ]' //
+global.footer = '[ ◉ ꪶ𝚅𝙸𝙺𝚄 𖠂 𝙻𝙸𝙾𝙽 ᴷᴵᴺᴳ_ꫂ ]' //
 
 //database
-global.premium = ['+380944814219'] //ur premium numbers
+global.premium = ['+94741018442'] //ur premium numbers
 
 //other
 global.sessionName = 'session'
@@ -71,7 +71,7 @@ global.mess = {
     group: 'Feature Used Only For Groups!',
     private: 'This Features Only For Private Chat!',
     bot: 'This Feature Is Only For Bot😝',
-    wait: '👉👈Pleas Wait...',
+    wait: '👉👑ᴠɪᴋᴜᴍࣻࣻࣻࣻᵏⁱⁿᵍـــ٨ـہہـ♡ـ👑👈Pleas Wait...',
     error: 'Error!!! Maybe Api Key Is Expired🤔!',
     endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
     linkm: 'Where is the link?',
